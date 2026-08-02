344b29e39a81010be11de8b2067c8b4fc1b1d0e5
Weslley Soares <weslley.soares.uol@gmail.com>
2026-08-02 16:12:44 -0300

    fix(cloudflare): import compiled dist/server/server.js in Pages Functions handler

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,4 +1,5 @@
-import server from "../src/server";
+// @ts-ignore
+import server from "../dist/server/server.js";
 
 export const onRequest: PagesFunction = async (context) => {
   return server.fetch(context.request, context.env, context);
